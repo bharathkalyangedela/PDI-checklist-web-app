5954a92c68106bb1e0047efbc58816390af2c405
Blyb22 <bharatbalu3@gmail.com>
2024-10-09 17:13:15 +0530

    Add Flask application structure and initial database setup, Login register Setup

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+# Import your routes here to avoid circular imports
+from app import routes
+
+# Create the database tables if they don't exist
+with app.app_context():
+    db.create_all()  # This line creates the database tables
